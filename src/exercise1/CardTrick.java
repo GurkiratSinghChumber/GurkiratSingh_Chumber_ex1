c9ad868154bf80b2877f279cd1dbb3ea981d235d
GurkiratSinghChumber <156931563+GurkiratSinghChumber@users.noreply.github.com>
2024-01-31 09:39:04 -0500

    Update CardTrick.java

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -90,7 +90,7 @@ public class CardTrick {
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
         
-        System.out.println("My name is Paul, but you can call me prof, Paul or sir");
+        System.out.println("My name is Gurkirat Singh Chumber, You can call me kirat");
         System.out.println();
         
         System.out.println("My career ambitions:");
